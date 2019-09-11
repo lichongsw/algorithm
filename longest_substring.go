dbe39a5904654f4893a6b6d7327a24b1cecd2ad6
lichongsw <lichong2@kingsoft.com>
2019-09-11 16:42:05 +0800

    Add a new dp equation

--- longest_substring.go
+++ longest_substring.go
@@ -18,6 +18,48 @@ func GetThreeMax(first int, second int, third int) int {
 	return max
 }
 
+// a better one to understand via dp
+func LongestSubStringBetter(first string, second string) (int, string) {
+	// status init
+	status := make([][]int, len(first))
+	for i := 0; i < len(first); i++ {
+		status[i] = make([]int, len(second))
+	}
+
+	// first row and column
+	for i := 0; i < len(second); i++ {
+		if first[0] == second[i] {
+			status[0][i] = 1
+		}
+	}
+
+	for i := 0; i < len(first); i++ {
+		if second[0] == first[i] {
+			status[i][0] = 1
+		}
+	}
+
+	maxStringLen := 0
+	index := 0
+
+	for i := 1; i < len(first); i++ {
+		for j := 1; j < len(second); j++ {
+			if first[i] != second[j] {
+				status[i][j] = 0
+			} else {
+				status[i][j] = status[i-1][j-1] + 1
+				if status[i][j] > maxStringLen {
+					maxStringLen = status[i][j]
+					index = i
+				}
+			}
+		}
+	}
+
+	s := string([]byte(first)[index+1-maxStringLen : index+1])
+	return maxStringLen, s
+}
+
 func LongestSubString(first string, second string) (int, string) {
 	// status init
 	status := make([][]int, len(first))
@@ -76,8 +118,12 @@ func main() {
 	secondString := "abcefgh"
 
 	length, substring := LongestSubString(firstString, secondString)
-	fmt.Println("The longest sub string for", firstString, "and", secondString, ",result:", substring, "with length:", length)
+	fmt.Println("Search longest sub string for", firstString, "and", secondString, ",result:", substring, "with length:", length)
+
+	length, substring = LongestSubStringBetter(firstString, secondString)
+	fmt.Println("The better search longest sub string for", firstString, "and", secondString, ",result:", substring, "with length:", length)
 }
 
 // Welcome to the playground!
-// The longest sub string for bdefg and abcefgh ,result: efg with length: 3
+// Search longest sub string for bdefg and abcefgh ,result: efg with length: 3
+// The better search longest sub string for bdefg and abcefgh ,result: efg with length: 3
