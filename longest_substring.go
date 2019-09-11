e105945105bfc86f60a740e0f845858b3dc6d49a
lichongsw <lichong2@kingsoft.com>
2019-09-11 18:02:52 +0800

    Add sub sequence solution

--- longest_substring.go
+++ longest_substring.go
@@ -4,22 +4,17 @@ import (
 	"fmt"
 )
 
-func GetThreeMax(first int, second int, third int) int {
+func GetMax(first int, second int) int {
 	max := first
 
 	if max < second {
 		max = second
 	}
 
-	if max < third {
-		max = third
-	}
-
 	return max
 }
 
-// a better one to understand via dp
-func LongestSubStringBetter(first string, second string) (int, string) {
+func LongestSubString(first string, second string) (int, string) {
 	// status init
 	status := make([][]int, len(first))
 	for i := 0; i < len(first); i++ {
@@ -60,7 +55,7 @@ func LongestSubStringBetter(first string, second string) (int, string) {
 	return maxStringLen, s
 }
 
-func LongestSubString(first string, second string) (int, string) {
+func LongestSubSequence(first string, second string) (int, string) {
 	// status init
 	status := make([][]int, len(first))
 	for i := 0; i < len(first); i++ {
@@ -68,14 +63,24 @@ func LongestSubString(first string, second string) (int, string) {
 	}
 
 	// first row and column
+	matched := false
 	for i := 0; i < len(second); i++ {
 		if first[0] == second[i] {
+			status[0][i] = 1
+			matched = true
+		}
+		if matched {
 			status[0][i] = 1
 		}
 	}
 
+	matched = false
 	for i := 0; i < len(first); i++ {
 		if second[0] == first[i] {
+			status[i][0] = 1
+			matched = true
+		}
+		if matched {
 			status[i][0] = 1
 		}
 	}
@@ -86,18 +91,10 @@ func LongestSubString(first string, second string) (int, string) {
 	// dp equation
 	for i := 1; i < len(first); i++ {
 		for j := 1; j < len(second); j++ {
-			subFind := 0
-			for first[i-subFind] == second[j-subFind] {
-				subFind++
-				if subFind > i || subFind > j {
-					break
-				}
-			}
-
-			if subFind > 0 {
-				status[i][j] = GetThreeMax(subFind, status[i-1][j], status[i][j-1])
+			if first[i] == second[j] {
+				status[i][j] = status[i-1][j-1] + 1
 			} else {
-				status[i][j] = GetThreeMax(status[i-1][j-1], status[i-1][j], status[i][j-1])
+				status[i][j] = GetMax(status[i-1][j], status[i][j-1])
 			}
 
 			if status[i][j] > maxStringLen {
@@ -107,23 +104,31 @@ func LongestSubString(first string, second string) (int, string) {
 		}
 	}
 
-	s := string([]byte(first)[index+1-maxStringLen : index+1])
-	return maxStringLen, s
+	result := make([]byte, 0)
+	index = 0
+	for i := 0; i < len(first); i++ {
+		if status[i][len(second)-1] > index {
+			result = append(result, first[i])
+			index++
+		}
+	}
+
+	return maxStringLen, string(result)
 }
 
 func main() {
 	fmt.Println("Welcome to the playground!")
 
-	firstString := "bdefg"
+	firstString := "bdefgih"
 	secondString := "abcefgh"
 
-	length, substring := LongestSubString(firstString, secondString)
+	length, substring := LongestSubSequence(firstString, secondString)
 	fmt.Println("Search longest sub string for", firstString, "and", secondString, ",result:", substring, "with length:", length)
 
-	length, substring = LongestSubStringBetter(firstString, secondString)
+	length, substring = LongestSubString(firstString, secondString)
 	fmt.Println("The better search longest sub string for", firstString, "and", secondString, ",result:", substring, "with length:", length)
 }
 
 // Welcome to the playground!
-// Search longest sub string for bdefg and abcefgh ,result: efg with length: 3
-// The better search longest sub string for bdefg and abcefgh ,result: efg with length: 3
+// Search longest sub string for bdefgih and abcefgh ,result: befgh with length: 5
+// The better search longest sub string for bdefgih and abcefgh ,result: efg with length: 3
