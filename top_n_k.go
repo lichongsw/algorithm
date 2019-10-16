2e477de7f9d1c8225e5cc21ac28673f28a8ebb5b
lichongsw <lichong2@kingsoft.com>
2019-10-16 10:37:56 +0800

    Add scope result for top N K

--- top_n_k.go
+++ top_n_k.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-func n_k(a []int, n int, k int) int {
-	if n < 2 && n > 0 && n >= k {
-		return a[0]
+func n_k_item(a []int, n int, k int) (int, bool) {
+	if n < k || n <= 0 {
+		return 0, false
 	}
 
 	left, right := 0, n-1
@@ -21,18 +21,53 @@ func n_k(a []int, n int, k int) int {
 	a[left], a[right] = a[right], a[left]
 
 	if k == n-left {
-		return a[left]
+		return a[left], true
 	} else if k < n-left {
-		return n_k(a[left+1:], len(a[left+1:]), k)
+		return n_k_item(a[left+1:], len(a[left+1:]), k)
 	} else {
-		return n_k(a[:left], len(a[:left]), k-(n-left))
+		return n_k_item(a[:left], len(a[:left]), k-(n-left))
+	}
+}
+
+func n_k_scope(a []int, n int, k int) ([]int, bool) {
+	if n < k || n <= 0 {
+		return nil, false
+	}
+
+	left, right := 0, n-1
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	if k == n-left {
+		return a[left:], true
+	} else if k < n-left {
+		return n_k_scope(a[left+1:], len(a[left+1:]), k)
+	} else {
+		result := make([]int, len(a[left:]))
+		copy(result, a[left:])
+		additional, _ := n_k_scope(a[:left], len(a[:left]), k-(n-left))
+		return append(result, additional...), true
 	}
 }
 
 func main() {
 	fmt.Println("Welcome to the playground!")
 
-	inputSlice := []int{1, 3, 5, 3, 2, 4, 6, 8, 4, 5, 6}
+	inputSlice := []int{1, 3, 5, 3, 2, 4, 6, 8, 4, 5, 7}
+	k, _ := n_k_item(inputSlice, len(inputSlice), 4)
+	fmt.Println("Result for item:", k)
 
-	fmt.Println("Result:", n_k(inputSlice, len(inputSlice), 8))
+	kscope, _ := n_k_scope(inputSlice, len(inputSlice), 4)
+	fmt.Println("Result for scope:", kscope)
 }
+
+// Welcome to the playground!
+// Result for item: 5
+// Result for scope: [8 7 6 5]
